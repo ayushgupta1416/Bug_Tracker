a3bf2cc554958c3922de22f908ad640b4e105bda
ayushgupta1416 <ayushgupta1416@gmail.com>
2020-08-13 16:40:21 +0530

    added user auth

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = 5000;
+
+const auth = require('./routes/auth');
+//const bugs = require('./routes/bugs');
+
+
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to database."))
+  .catch(() => console.log("Failed to connect to database."));
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api/user', auth);
+
+
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
